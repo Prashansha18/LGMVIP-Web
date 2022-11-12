4714586bf0d3bc2089f47aeed74c8249a38d1dae
Prashansha18 <116421781+Prashansha18@users.noreply.github.com>
2022-11-12 19:05:53 +0530

    Create App.js

---
+++ Task2/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./App.css";
+import Home from "./Home";
+import Load from "./Load";
+import Card from "./Card";
+
+function App() {
+  const [fetchData, setfetchData] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [load, setLoad] = useState(false);
+
+  const getData = async () => {
+    setLoad(true);
+    const response = await fetch("https://reqres.in/api/users?page=1");
+    const res = await response.json();
+    setUsers(res.data);
+    setInterval(() => {
+      setfetchData(true);
+    }, 2000);
+  };
+
+  return (
+    <>
+      {load ? (
+        fetchData ? (
+          <Card users={users} />
+        ) : (
+          <Load />
+        )
+      ) : (
+        <Home getData={getData} />
+      )}
+    </>
+  );
+}
+
+export default App;
