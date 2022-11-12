d90e1d0253ce78a082c0499775fa2ebbf0035fa0
Prashansha18 <116421781+Prashansha18@users.noreply.github.com>
2022-11-12 19:13:45 +0530

    Create Test.js

---
+++ Task2/src/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+export default function Load() {
+  return (
+    <section>
+      <svg>
+        <circle cx="70" cy="70" r="70"></circle>
+      </svg>
+    </section>
+  );
+}
